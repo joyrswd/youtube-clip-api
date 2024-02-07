92f21d1022fef66692660570edb047db173789fe
joyrswd <5625893+joyrswd@users.noreply.github.com>
2024-02-07 22:14:37 +0900

    チャンネル登録バッチ実装

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -25,10 +25,28 @@ class Video extends Model
     {
         return [
             'id' => $this->id,
+            'url' => 'https://www.youtube.com/watch?v=' . $this->youtube_id,
+            'thumbnail' => 'https://i.ytimg.com/vi/' . $this->youtube_id . '/default.jpg',
             'title' => $this->title,
-            'description' => $this->description,
+            'description' => $this->trimUrl($this->description),
             'channel' => $this->channel->title,
             'tags' => $this->tags->pluck('name'),
+            'duration' => $this->duration,
+            'time'  => $this->convertToTime($this->duration),
+            'published_at' => $this->published_at,
+            'timesatmp' => strtotime($this->published_at),
         ];
+    }
+
+    private function trimUrl(string $description) : string
+    {
+        return preg_replace('/[^\n]*(?:https?|ftp):\/\/\S+/', '', $description);
+    }
+
+    private function convertToTime($duration)
+    {
+        $start = new \DateTime('@0');
+        $start->modify("+ {$duration} seconds");
+        return ($duration >= 3600) ? $start->format('H:i:s') : $start->format('i:s');
     }
 }
